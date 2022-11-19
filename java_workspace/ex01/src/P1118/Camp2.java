730ed89765b47a4574cef7f4eaa055c098c46b05
jinseon5462 <dlwlstjs5462@gmail.com>
2022-11-19 16:14:53 +0900

    2022.11.19 rename project

---
+++ java_workspace/ex01/src/P1118/Camp2.java
@@ -0,0 +1,16 @@
+package P1118;
+
+public class Camp2 {
+    private Object unit; //human, machine 모두 object 하나로 받기(상속) 가능
+    // **object는 모든 클래스의 부모이다.
+
+    public void set(Object unit) {
+        this.unit = unit;
+    }
+
+    public Object get() {
+        return unit;
+    }
+
+
+}
---
+++ java_workspace/ex01/src/P1118/Npc1.java
@@ -0,0 +1,9 @@
+package P1118;
+
+public class Npc1 {
+
+    public String toString(){
+        return "This is a Npc1";
+    }
+
+}
